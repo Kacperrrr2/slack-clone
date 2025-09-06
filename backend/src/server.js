da09f7da9fdc476cba13c5d6b908acbebd12ce7b
kacper <k4cper.petelicki@gmail.com>
2025-09-06 14:38:12 +0200

    connect monogoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { ENV } from './config/env.js';
+import { connectDB } from './config/db.js';
 
 
 
@@ -8,4 +9,11 @@ const app = express();
 app.get('/', (req, res) => res.send('Hello World!'));
 
 console.log(ENV.MONGO_URI);
-app.listen(ENV.PORT, () => console.log(`Listening on po ${ENV.PORT}`));
+app.listen(ENV.PORT, () => {
+    console.log(`Listening on po ${ENV.PORT}`)
+    connectDB();
+
+});
+
+
+
